23a4fc737033420a4a2234abb0c5fbef1e62245b
FreedomWriter <natalienetworks@gmail.com>
2020-01-24 23:29:14 -0600

    I just want to push these styles

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  const [projects, setProjects] = useState([]);
+  useEffect(() => {
+    axios
+      .get(`http://localhost:4000/api/projects/`)
+      .then(res => setProjects(res.data.projects))
+      .catch(err => console.log(err));
+  }, []);
+  console.log(projects);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+      {projects.map(project => {
+        return (
+          <div key={project.id} className="app">
+            <h2>Project: </h2>
+            <p>{project.name}</p>
+            <h2>Project Description: </h2>
+            <p>{project.description}</p>
+          </div>
+        );
+      })}
     </div>
   );
 }
