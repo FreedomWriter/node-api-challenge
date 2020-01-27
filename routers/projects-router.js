5badf6fef636e533dfa16ef52f9db877f1385b5e
FreedomWriter <natalienetworks@gmail.com>
2020-01-27 09:08:12 -0600

    separated actions routes

--- routers/projects-router.js
+++ routers/projects-router.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const actionsRouter = require("./actions-router");
 
 const db = require("../data/helpers/projectModel");
 const actionsDb = require("../data/helpers/actionModel");
@@ -7,6 +8,8 @@ const validateProjectId = require("../middlewares/validateID");
 const validator = require("../middlewares/validator");
 
 const router = express.Router();
+
+router.use("/:id/actions", actionsRouter);
 
 router.get("/", (req, res) => {
   db.get()
@@ -64,90 +67,6 @@ router.delete("/:id", validateProjectId, (req, res) => {
       res.status(500).json({
         success: false,
         error: "Could not delete project.",
-        errorMessage
-      })
-    );
-});
-
-//nested route to get actions for a project
-router.get("/:id/actions", validateProjectId, (req, res) => {
-  const { id } = req.params;
-  db.getProjectActions(id)
-    .then(actions => res.status(200).json({ success: true, actions }))
-    .catch(err =>
-      res.status(500).json({
-        success: false,
-        error: "Could not get project actions.",
-        errorMessage
-      })
-    );
-});
-
-//nested route to add actions to a project
-router.post(
-  "/:id/actions",
-  validator("notes"),
-  validator("description"),
-  (req, res) => {
-    const { id } = req.params;
-    actionsDb
-      .insert({ ...req.body, project_id: id })
-      .then(project => res.status(200).json({ success: true, project }))
-      .catch(err =>
-        res.status(500).json({
-          success: false,
-          error: "Could not add project.",
-          errorMessage
-        })
-      );
-  }
-);
-
-//nested route to get actions to a project
-router.get("/:id/actions", validateProjectId, (req, res) => {
-  const { id } = req.params;
-  actionsDb
-    .get(id)
-    .then(actions => res.status(200).json({ success: true, actions }))
-    .catch(err =>
-      res
-        .status(500)
-        .json({ success: false, errorMessage: "Could not get actions", err })
-    );
-});
-
-//nested route to update actions on a project
-router.put("/:id/actions/:actionsId", validateProjectId, (req, res) => {
-  const { id } = req.params;
-  const { body } = req;
-  const { actionsId } = req.params;
-  console.log(id, body, actionsId);
-  actionsDb
-    .update(actionsId, { ...req.body, project_id: id })
-    .then(project => res.status(201).json({ success: true, project }))
-    .catch(err =>
-      res.status(500).json({
-        success: false,
-        error: "Could not update actions.",
-        errorMessage
-      })
-    );
-});
-
-//nested route to delete actions on a project
-
-router.delete("/:id/actions/:actionsId", validateProjectId, (req, res) => {
-  const { id } = req.params;
-  const { body } = req;
-  const { actionsId } = req.params;
-  console.log(id, body, actionsId);
-  actionsDb
-    .remove(actionsId)
-    .then(project => res.status(201).json({ success: true, project }))
-    .catch(err =>
-      res.status(500).json({
-        success: false,
-        error: "Could not delete action.",
         errorMessage
       })
     );
